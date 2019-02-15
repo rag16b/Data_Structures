0bef1f7086f9c9e3c082f9c53c3ef4618d263a86
pyrokuna <31514534+pyrokuna@users.noreply.github.com>
2019-02-15 01:02:23 -0500

    First proj2 commit
    Need to fix global operator==, and reverse() in List class.

---
+++ proj2/List.hpp
@@ -0,0 +1,282 @@
+//-------LIST.HPP-------
+//
+// Ryan Gutierrez
+// COP4530 - 0001
+// Project 2
+// Understanding generic programming and information hiding by 
+//  developing generic containers. Getting familiar with the 
+//  concept of class template and its usage. Use of nested 
+//  (iterator) classes. Use of namespace. Operator overloading.
+
+#include "List.h"
+
+// CONST_ITERATOR MEMBERS-------------------------------------------
+template <typename T>
+List<T>::const_iterator::const_iterator() : current{ nullptr }
+	{}
+template <typename T>
+const T & List<T>::const_iterator::operator*() const 
+	{ return retrieve(); }
+
+template <typename T>				// don't know what the return type should be for inc and dec
+typename List<T>::const_iterator & List<T>::const_iterator::operator++(){
+	current = current->next;
+	return *this; 
+}
+template <typename T>
+typename List<T>::const_iterator List<T>::const_iterator::operator++(int){
+	const_iterator old = *this;
+	++(*this);
+	return old;
+}
+template <typename T>
+typename List<T>::const_iterator & List<T>::const_iterator::operator--(){
+	current = current->prev;
+	return *this; 
+}
+template <typename T>
+typename List<T>::const_iterator List<T>::const_iterator::operator--(int){
+	const_iterator old = *this;
+	--(*this);
+	return old;
+}
+
+template <typename T>
+bool List<T>::const_iterator::operator==(const const_iterator &rhs) const
+	{ return current == rhs.current; }
+template <typename T>
+bool List<T>::const_iterator::operator!=(const const_iterator &rhs) const
+	{ return !(*this == rhs.current); }
+
+template <typename T>
+T & List<T>::const_iterator::retrieve() const
+	{ return current->data; }
+template <typename T>
+List<T>::const_iterator::const_iterator( Node *p ) : current{ p }
+	{}
+// end const_iterator member function implementations---------------
+
+// ITERATOR MEMBERS-------------------------------------------------
+template <typename T>
+List<T>::iterator::iterator()
+	{}
+template <typename T>
+T & List<T>::iterator::operator*()
+	{ return const_iterator::retrieve(); }
+template <typename T>
+const T & List<T>::iterator::operator*() const
+	{ return const_iterator::operator*(); }
+
+template <typename T>
+typename List<T>::iterator & List<T>::iterator::operator++(){
+	this->current = this->current->next;
+	return *this;
+}
+template <typename T>
+typename List<T>::iterator List<T>::iterator::operator++(int){
+	iterator old = *this;
+	++(*this);
+	return old;
+}
+template <typename T>
+typename List<T>::iterator & List<T>::iterator::operator--(){
+	this->current = this->current->prev;
+	return *this;
+}
+template <typename T>
+typename List<T>::iterator List<T>::iterator::operator--(int){	// try calling const_iterator version of this
+	iterator old = *this;
+	--(*this);
+	return old;
+}
+
+template <typename T>
+List<T>::iterator::iterator( Node *p ) : const_iterator{ p }
+	{}
+// end iterator member function implementations---------------------
+
+// LIST MEMEBERS----------------------------------------------------
+template <typename T>
+List<T>::List()
+	{ init(); }
+template <typename T>
+List<T>::List(const List &rhs){
+	init();
+	for ( auto & x : rhs )
+		push_back( x );
+}
+template <typename T>
+List<T>::List(List && rhs) : theSize{ rhs.theSize }, head{ rhs.head }, tail{ rhs.tail }{
+	rhs.theSize = 0;
+	rhs.head = nullptr;
+	rhs.tail = nullptr;
+}
+template <typename T>		// make sure this is still working correctly***********************
+List<T>::List(int num, const T & val){
+	init();
+	for (int i = 0; i < num; i++)
+		push_back(val);
+}
+template <typename T>
+List<T>::List(const_iterator start, const_iterator end){
+	init();
+	while(start != end)
+		push_back((start++).current->data);
+}
+
+template <typename T>
+List<T>::~List(){
+	clear();
+	delete head;
+	delete tail;
+}
+
+template <typename T>
+const List<T> & List<T>::operator=(const List<T> &rhs){
+	List copy = rhs;
+	std::swap( *this, copy );
+	return *this;
+}
+template <typename T>
+List<T> & List<T>::operator=( List && rhs){
+	std::swap( theSize, rhs.theSize );
+	std::swap( head, rhs.head );
+	std::swap( tail, rhs.tail );
+}
+
+template <typename T>
+int List<T>::size() const
+	{ return theSize; }
+template <typename T>
+bool List<T>::empty() const
+	{ return size() == 0; }
+template <typename T>
+void List<T>::clear(){
+	while( !empty() )
+		pop_front();
+}
+template <typename T>	//*************************************************************************
+void List<T>::reverse(){
+	
+}
+
+template <typename T>
+T & List<T>::front()
+	{ return *begin(); }
+template <typename T>
+const T & List<T>::front() const
+	{ return *begin(); }
+template <typename T>
+T & List<T>::back()
+	{ return *--end(); }
+template <typename T>
+const T & List<T>::back() const
+	{ return *--end(); }
+
+template <typename T>
+void List<T>::push_front(const T & val)
+	{ insert(begin(),val); }
+template <typename T>
+void List<T>::push_front(T && val)
+	{ insert(begin(),std::move(val)); }
+template <typename T>
+void List<T>::push_back(const T & val)
+	{ insert(end(),val); }
+template <typename T>
+void List<T>::push_back(T && val)
+	{ insert(end(),std::move(val)); }
+template <typename T>
+void List<T>::pop_front()
+	{ erase(begin()); }
+template <typename T>
+void List<T>::pop_back()
+	{ erase(--end()); }
+
+template <typename T>	//*************************************************************************
+void List<T>::remove(const T & val){
+	for( List<T>::iterator temp = begin(); temp != end(); temp++ )
+		if( temp.current->data == val )
+			erase(temp);
+}
+
+template <typename T>// maybe	//*****************************************************************
+void List<T>::print(std::ostream & os, char ofc) const{
+	for( List<T>::const_iterator temp = begin(); temp != end(); temp++ )
+		os << temp.current->data << ofc;
+}
+
+template <typename T>
+typename List<T>::iterator List<T>::begin()
+	{ return { head->next }; }
+template <typename T>
+typename List<T>::const_iterator List<T>::begin() const
+	{ return { head->next }; }
+template <typename T>
+typename List<T>::iterator List<T>::end()
+	{ return { tail }; }
+template <typename T>
+typename List<T>::const_iterator List<T>::end() const
+	{ return { tail }; }
+template <typename T>
+typename List<T>::iterator List<T>::insert( List<T>::iterator itr, const T& val ){
+	Node *p = itr.current;
+	theSize++;
+	return { p->prev = p->prev->next = new Node{val,p->prev,p} };
+}
+template <typename T>
+typename List<T>::iterator List<T>::insert( List<T>::iterator itr, T && val ){
+	Node *p = itr.current;
+	theSize++;
+	return { p->prev = p->prev->next = new Node{std::move(val),p->prev,p} };	
+}
+template <typename T>
+typename List<T>::iterator List<T>::erase( List<T>::iterator itr ){
+	Node *p = itr.current;
+	iterator retVal{ p->next };
+	p->prev->next = p->next;
+	p->next->prev = p->prev;
+	delete p;
+	theSize--;
+	return retVal;
+}
+template <typename T>
+typename List<T>::iterator List<T>::erase( List<T>::iterator start, iterator end ){
+	for( iterator itr = start; itr != end; )
+		itr = erase( itr );
+	return end;
+}
+
+template <typename T>
+void List<T>::init(){
+	theSize = 0;
+	head = new Node;
+	tail = new Node;
+	head->next = tail;
+	tail->prev = head;
+}
+// end List function implementations--------------------------------
+
+// GLOBAL FUNCTION DEFINITIONS--------------------------------------
+template <typename T>
+bool operator==(const List<T> & lhs, const List<T> & rhs){
+	if (lhs.size() == rhs.size()){
+		typename List<T>::iterator itrL = lhs.begin();
+		typename List<T>::iterator itrR = rhs.begin();
+		for (int i = 0; i < lhs.size(); i++){
+			if ((itrL++).current->data != (itrR++).current->data)
+				return false;
+		}
+		return true;
+	}
+	return false;
+}
+template <typename T>
+bool operator!=(const List<T> & lhs, const List<T> & rhs){
+	return !(lhs == rhs);
+}
+template <typename T>
+std::ostream & operator<<(std::ostream & os, const List<T> & l){
+	l.print(os);
+	return os;
+}
+// end global function implementations------------------------------
