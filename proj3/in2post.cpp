99e5216b500337f25ce36e76059f894d2d8e4a94
pyrokuna <31514534+pyrokuna@users.noreply.github.com>
2019-03-07 22:57:32 -0500

    proj3 : in2post.cpp update
    in2post.cpp skeleton work finished

---
+++ proj3/in2post.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include "stack.h"
+
+using namespace std;
+using namespace cop4530;
+
+int main(){
+
+	string in, post;	// in takes input (either an expression or "exit"). post will hold the final expression
+	string operand;		// current character
+	Stack<char> tokens;	// stack for evaluating postfix expressions;
+	
+	cout << "Enter infix expression (\"exit\" to quit): ";
+	cin >> in;
+
+	while(inp != "exit"){
+		cout << "Enter infix expression (\"exit\" to quit): ";
+		cin >> in;
+		
+		post = ""	// resetting
+
+		// converting from infix to postfix
+		
+	}
+
+	return 0;
+}
